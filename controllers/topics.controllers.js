ca770268cd303df3bc4a7cc0912fd5cde1363740
Ima <imakhan@Imas-MacBook-Air.local>
2023-11-22 10:04:25 +0000

    delete comment code

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -13,19 +13,3 @@ exports.getDocs = (req, res) => {
   res.status(200).send({ endpoints });
 };
 
-// exports.getArticlesById = (req, res, next) => {
-//   const { article_id } = req.params;
-//   selectArticleById(article_id)
-//     .then((article) => {
-//       res.status(200).send({ article });
-//     })
-//     .catch(next);
-// };
-
-// exports.getAllArticles = (req, res, next) => {
- 
-//   selectArticles().then((articles) => {
-//     res.status(200).send({ articles });
-//   })
-//   .catch(next)
-// };
